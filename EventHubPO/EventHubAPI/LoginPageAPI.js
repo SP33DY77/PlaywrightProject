91031e5979025e9dd713c497c5b0917ec440ea1d
SP33DY77 <kiraga77@wp.pl>
2026-07-19 10:04:38 +0200

    Path fix v4

--- EventHubPO/EventHubAPI/LoginPageAPI.js
+++ EventHubPO/EventHubAPI/LoginPageAPI.js
@@ -15,7 +15,6 @@ class LoginPageAPI {
         } else {
             throw new Error(`Failed to get token: ${JSON.stringify(loginRespJson)}`);
         }
-        console.log("TOKEN SET", token);
     }
 }
 module.exports = LoginPageAPI;
