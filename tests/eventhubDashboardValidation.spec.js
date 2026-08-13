d6e9eb26c07b6880afe86261166f1ac8211408ae
SP33DY77 <kiraga77@wp.pl>
2026-08-13 22:41:53 +0200

    DashboardValidation added

---
+++ tests/eventhubDashboardValidation.spec.js
@@ -0,0 +1,25 @@
+const { test, expect, request } = require("@playwright/test");
+const LoginPageAPI = require("../EventHubPO/EventHubAPI/LoginPageAPI");
+const LoginPage = require("../EventHubPO/LoginPage");
+const baseURLAPI = "https://api.eventhub.rahulshettyacademy.com/api/";
+const loginPayload = require("../EventHubPO/LoginPayload.json");
+const AuthHelper = require("../EventHubPO/EventHubAPI/AuthHelper");
+let loginToken;
+
+
+test.beforeAll(async () => {
+    const apiContext = await request.newContext();
+    const loginPageAPI = new LoginPageAPI(apiContext, loginPayload, baseURLAPI + "auth/login")
+    loginToken = await loginPageAPI.getToken();
+    
+});
+
+
+test("Verify events on dashboard", async ({ page }) => {
+    const authHelper = new AuthHelper(page, loginToken);
+    await authHelper.loginToPage();
+    const loginPage = new LoginPage(page);
+    await loginPage.goToBaseURL();
+    await expect(page).toHaveURL("/")
+
+});
